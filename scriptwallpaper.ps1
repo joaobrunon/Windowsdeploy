9c938a8940bf9fc864eac5420807c52f5ebccc0a
joaobrunon <47648366+joaobrunon@users.noreply.github.com>
2023-08-08 00:06:46 -0300

    Update scriptwallpaper.ps1

--- scriptwallpaper.ps1
+++ scriptwallpaper.ps1
@@ -1,13 +1,21 @@
 # Cria a pasta proatec em AppData\Local
-New-Item -Path "$env:APPDATA\Local" -Name "proatec" -ItemType "directory"
+Write-Output "Criando pasta proatec..."
+$folderPath = New-Item -Path "$env:LOCALAPPDATA\" -Name "proatec" -ItemType "directory"
+Write-Output "Pasta proatec criada em: $($folderPath.FullName)"
 
 # Baixa o arquivo para a pasta proatec
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/joaobrunon/Windowsdeploy/main/wallpaper.bat?token=GHSAT0AAAAAACF3B3A6GD4TBDD2AB5UNSCOZGRVNDA' -OutFile "$env:APPDATA\Local\proatec\wallpaper.bat"
+Write-Output "Baixando arquivo wallpaper.bat..."
+$downloadPath = Join-Path -Path $folderPath.FullName -ChildPath "wallpaper.bat"
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/joaobrunon/Windowsdeploy/main/wallpaper.bat' -OutFile $downloadPath
+Write-Output "Arquivo wallpaper.bat baixado para: $downloadPath"
 
 # Executa o arquivo wallpaper.bat
-Start-Process -FilePath "$env:APPDATA\Local\proatec\wallpaper.bat"
+Write-Output "Executando arquivo wallpaper.bat..."
+$process = Start-Process -FilePath $downloadPath -PassThru
+Write-Output "Arquivo wallpaper.bat executado. ID do processo: $($process.Id)"
 
 # Define o plano de fundo da área de trabalho como a imagem padrão do Windows
+Write-Output "Definindo plano de fundo da área de trabalho..."
 Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
@@ -20,9 +28,4 @@ public class Wallpaper {
 
 $wallpaperPath = "C:\Windows\Web\Wallpaper\Windows\img0.jpg"
 [Wallpaper]::SystemParametersInfo(20, 0, $wallpaperPath, 3)
-
-# Cria um atalho para a pasta proatec na área de trabalho
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Desktop\proatec.lnk")
-$Shortcut.TargetPath = "$env:APPDATA\Local\proatec"
-$Shortcut.Save()
+Write-Output "Plano de fundo da área de trabalho definido para: $wallpaperPath"
