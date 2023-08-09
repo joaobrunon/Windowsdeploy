92a96f250a23a96343bec6d8f1a354fb67b917e6
joaobrunon <47648366+joaobrunon@users.noreply.github.com>
2023-08-09 01:03:44 -0300

    Update scriptwallpaper.ps1

--- scriptwallpaper.ps1
+++ scriptwallpaper.ps1
@@ -1,38 +1,52 @@
+# Define o caminho para o arquivo de log
+$logPath = Join-Path -Path $PSScriptRoot -ChildPath "wallpaper_log.txt"
+
+function Write-LogOutput {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Message
+    )
+
+    Write-Output $Message | Out-File -Append -FilePath $logPath
+}
+
 # Instala o Windows Package Manager (winget)
-Write-Output "Baixando e instalando o Windows Package Manager..."
+Write-LogOutput "Baixando e instalando o Windows Package Manager..."
 $wingetInstallerUrl = "https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 Invoke-WebRequest -Uri $wingetInstallerUrl -OutFile "$env:TEMP\winget.msixbundle"
 Add-AppPackage -Path "$env:TEMP\winget.msixbundle"
-Write-Output "Windows Package Manager instalado."
+Write-LogOutput "Windows Package Manager instalado."
 
 # Verifica se o Chocolatey já está instalado
 if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
     # Instala o Chocolatey
-    Write-Output "Instalando o Chocolatey..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    Write-Output "Chocolatey instalado."
+    Write-LogOutput "Instalando o Chocolatey..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Write-LogOutput "Chocolatey instalado."
 } else {
-    Write-Output "O Chocolatey já está instalado."
+    Write-LogOutput "O Chocolatey já está instalado."
 }
 
 # Cria a pasta proatec em AppData\Local
-Write-Output "Criando pasta proatec..."
+Write-LogOutput "Criando pasta proatec..."
 $folderPath = New-Item -Path "$env:LOCALAPPDATA\" -Name "proatec" -ItemType "directory"
-Write-Output "Pasta proatec criada em: $($folderPath.FullName)"
+Write-LogOutput "Pasta proatec criada em: $($folderPath.FullName)"
 
 # Baixa o arquivo para a pasta proatec
-Write-Output "Baixando arquivo wallpaper.bat..."
+Write-LogOutput "Baixando arquivo wallpaper.bat..."
 $downloadPath = Join-Path -Path $folderPath.FullName -ChildPath "wallpaper.bat"
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/joaobrunon/Windowsdeploy/main/wallpaper.bat' -OutFile $downloadPath
-Write-Output "Arquivo wallpaper.bat baixado para: $downloadPath"
+Write-LogOutput "Arquivo wallpaper.bat baixado para: $downloadPath"
 
 # Executa o arquivo wallpaper.bat
-Write-Output "Executando arquivo wallpaper.bat..."
+Write-LogOutput "Executando arquivo wallpaper.bat..."
 $process = Start-Process -FilePath $downloadPath -PassThru
-Write-Output "Arquivo wallpaper.bat executado. ID do processo: $($process.Id)"
+Write-LogOutput "Arquivo wallpaper.bat executado. ID do processo: $($process.Id)"
 
 # Define o plano de fundo da área de trabalho como a imagem padrão do Windows
-Write-Output "Definindo plano de fundo da área de trabalho..."
+Write-LogOutput "Definindo plano de fundo da área de trabalho..."
 Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
@@ -45,4 +59,4 @@ public class Wallpaper {
 
 $wallpaperPath = "C:\Windows\Web\Wallpaper\Windows\img0.jpg"
 [Wallpaper]::SystemParametersInfo(20, 0, $wallpaperPath, 3)
-Write-Output "Plano de fundo da área de trabalho definido para: $wallpaperPath"
+Write-LogOutput "Plano de fundo da área de trabalho definido para: $wallpaperPath"
